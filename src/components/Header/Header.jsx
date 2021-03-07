5e66731065fc267755cd426cb8c3460b32f7ad14
Shurygina-Jul <melam.2916@gmail.com>
2021-03-07 18:32:31 +0300

    header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,44 @@ import './Header.css'
 
 const b = block('header')
 
-export const Header = () => (
+/**export const Header = () => (
   <header className={b()}>
     <span className={b('title')}>
       Application title
     </span>
   </header>
+)*/
+export const Header = () => (
+  <header>
+    <nav>
+      <ul>
+<li>About</li>
+<li>Aparment</li>
+<li>How it Work</li>
+<li>Agents</li>
+<li>Contact</li>
+<li>Join Us</li>
+<li>Getting Started</li>
+      </ul>
+      <span>☰</span>
+    </nav>
+  </header>
 )
+/**<header>
+<nav>
+<input type="checkbox" id="checkbox-menu" />
+<label for="checkbox-menu">
+  <ul class="menu touch">
+    <li><a class="logo" href="#">luxestate</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Apartment</a></li>
+    <li><a href="#">How it Work</a></li>
+    <li><a href="#">Agents</a></li>
+    <li><a class="contacts" href="#">Contact Us</a></li>
+    <li><a href="#">Join Us</a></li>
+    <li><a href="#">Getting Started</a></li>
+  </ul>
+  <span class="toggle">☰</span>
+</label>
+</nav>
+</header>*/
