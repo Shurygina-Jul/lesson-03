4a589d361879c29a3a1d7677e98fc72e32b6e362
Shurygina-Jul <melam.2916@gmail.com>
2021-03-09 00:33:39 +0300

    css-v2

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,18 +1,28 @@
 import block from 'bem-cn'
 import React from 'react'
 import './Content.css'
-const b = block('content')
+const b = block('content');
+const p = block('paragraf');
 
 
 export const Content = () => (
-  <section className={b()}>
-
-
+  <div className={b()}>
+<img className="image" src="https://th.bing.com/th/id/R3cf3dcd5141b6fd1f35fa711c194a4fb?rik=UPBj1tu7Imh5bA&riu=http%3a%2f%2fpngimg.com%2fuploads%2fmug_coffee%2fmug_coffee_PNG16843.png&ehk=6EyyZfA9X3LCYvhABm%2ffl2b%2bL9B%2fUGK%2fKCLM1RqMw0E%3d&risl=&pid=ImgRaw" alt="coffee"></img>
             <h4 className={b('h4')}>Renting</h4>
-            <p className={b('paragraf')}>
+            <div className={p()}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
               aspernatur, recusandae rerum maiores, fuga inventore expedita
               alias perspiciatis numquam itaque nemo laboriosam optio.
-            </p>
+            </div>
+            <div className={p()}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              aspernatur, recusandae rerum maiores, fuga inventore expedita
+              alias perspiciatis numquam itaque nemo laboriosam optio.
+            </div>
+            <div className={p()}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              aspernatur, recusandae rerum maiores, fuga inventore expedita
+              alias perspiciatis numquam itaque nemo laboriosam optio.
+            </div>
 
-        </section>)
+        </div>)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import block from 'bem-cn'
 import React from 'react'
 import './Header.css'
